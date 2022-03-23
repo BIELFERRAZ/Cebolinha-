e6d1dc47b1190b8b84533a0183960b4fe79bdab5
LAB-00\lab00-00 <e107ti@cps.sp.gov.br>
2022-03-23 18:28:47 -0300

    Finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string frase;
+
+Console.WriteLine("Por favor , Escreva uma Frase");
+frase = Console.ReadLine()!;
+
+string novafrase = frase
+.Replace ("r","l"!)
+.Replace ("R" , "L"! );
+
+Console.WriteLine(novafrase);
